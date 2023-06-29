4f88e9819a740b273bf3f8acfef8780f2ef06fff
abhi <pandeyabhi142002@gmail.com>
2023-06-29 22:46:36 +0530

    Added link to project cards

--- src/Components/ProjectSection.jsx
+++ src/Components/ProjectSection.jsx
@@ -1,25 +1,61 @@
+import podcastPlatformWebapp from "../assets/projectSection/podcastPlatformWebapp.png";
+import typingSpeedTest from "../assets/projectSection/typingSpeedTest.png";
+import youtubeClone from "../assets/projectSection/youtubeClone.png";
+import abhiPortfolio from "../assets/projectSection/abhiPortfolio.png";
 
-import podcastPlatformWebapp  from '../assets/projectSection/podcastPlatformWebapp.png'
-import typingSpeedTest from '../assets/projectSection/typingSpeedTest.png'
-import youtubeClone from '../assets/projectSection/youtubeClone.png'
-import abhiPortfolio from '../assets/projectSection/abhiPortfolio.png'
 const ProjectSection = () => {
-
-
-    return (
-        <div className="projectSection" id='projects'>
-            <h6>Projects <span style={{ fontSize: '35px', color: '#FE4E00' }}>•</span></h6>
-            <div className="projectMainContainer">
-                <div className="projectContainer projectContainer1"><img src={podcastPlatformWebapp}/></div>
-                <div className="projectContainer projectContainer2"><img src={typingSpeedTest}/></div>
-                <div className="projectContainer projectContainer3"><img src={youtubeClone}/></div>
-                <div className="projectContainer projectContainer4"><img src={abhiPortfolio}/></div>
-            </div>
+  return (
+    <div>
+      <div className="projectSection" id="projects">
+        <h6>
+          Projects <span style={{ fontSize: "35px", color: "#FE4E00" }}>•</span>
+        </h6>
+        <div className="projectMainContainer">
+          <div
+            className="projectContainer projectContainer1"
+            onClick={() => {
+              window.open(
+                "https://podcast-platform-webapp.netlify.app/",
+                "_blank"
+              );
+            }}
+          >
+            <img src={podcastPlatformWebapp} />
+          </div>
+          <div
+            className="projectContainer projectContainer2"
+            onClick={() => {
+              window.open(
+                "https://typing-speed-test-june.netlify.app/",
+                "_blank"
+              );
+            }}
+          >
+            <img src={typingSpeedTest} />
+          </div>
+          <div
+            className="projectContainer projectContainer3"
+            onClick={() => {
+              window.open(
+                "https://abhipandey1402.github.io/youtube-clone/",
+                "_blank"
+              );
+            }}
+          >
+            <img src={youtubeClone} />
+          </div>
+          <div
+            className="projectContainer projectContainer4"
+            onClick={() => {
+              window.open("https://abhipandey.netlify.app/", "_blank");
+            }}
+          >
+            <img src={abhiPortfolio} />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-
-export default ProjectSection
-
-
+export default ProjectSection;
